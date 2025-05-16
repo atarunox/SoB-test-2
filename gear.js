519a83f85c29d776c5816ebd57f8430bfc4f31ff
atarunox <ffxi.nox@hotmail.com>
2025-05-15 17:11:30 -0700

    Add files via upload

--- gear.js
+++ gear.js
@@ -1,6 +1,6 @@
 export const gearList = [
   { id: "sturdy_hat", name: "Sturdy Hat", slot: "Head", effects: { Defense: 1 } },
-  { id: "tinker_goggles", name: "Tinkerâ€™s Goggles", slot: "Head", effects: { Lore: 1, Cunning: 1 } },
+  { id: "tinker_goggles", name: "Tinker’s Goggles", slot: "Head", effects: { Lore: 1, Cunning: 1 } },
 
   { id: "leather_vest", name: "Leather Vest", slot: "Torso", effects: { Willpower: 1 } },
   { id: "iron_chestplate", name: "Iron Chestplate", slot: "Torso", effects: { Defense: 2 } },
@@ -9,7 +9,7 @@ export const gearList = [
   { id: "flamecoat", name: "Flamecoat", slot: "Coat", effects: { Lore: 1 }, darkStone: true },
 
   { id: "brass_knuckles", name: "Brass Knuckles", slot: "Gloves", effects: { Strength: 1 } },
-  { id: "alchemists_gloves", name: "Alchemistâ€™s Gloves", slot: "Gloves", effects: { Cunning: 1, Luck: 1 } },
+  { id: "alchemists_gloves", name: "Alchemist’s Gloves", slot: "Gloves", effects: { Cunning: 1, Luck: 1 } },
 
   { id: "steel_blade", name: "Steel Blade", slot: "Hands", effects: { Combat: 1 }, twoHanded: false },
   { id: "warhammer", name: "Warhammer", slot: "Hands", effects: { Combat: 2, Strength: 1 }, twoHanded: true },
