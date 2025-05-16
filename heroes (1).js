5c309a2c4f219c8391140053fd181e52ca31a338
atarunox <ffxi.nox@hotmail.com>
2025-05-15 23:33:39 -0700

    Add files via upload

---
+++ heroes (1).js
@@ -0,0 +1,11 @@
+// heroes.js - Western heroes
+const HEROES = {
+  "Western": {
+    "Trederran Veteran": { ... },
+    "Cowboy": { ... },
+    "Prospector": { ... },
+    "Jargono Native": { ... },
+    "Dark Stone Shaman": { ... },
+    "Drifter": { ... }
+  }
+};
