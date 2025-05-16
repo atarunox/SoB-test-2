4288ab24f70638ee81210bb1b5235ffc501d32a3
atarunox <ffxi.nox@hotmail.com>
2025-05-15 20:00:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-// Hero Tracker v0.1.02 - Draggable Character Sheet Panels with mobile support
-document.querySelector("h1")?.insertAdjacentHTML("beforeend", " <span style='font-size:0.7em'>(v0.1.02)</span>");
+// Hero Tracker v0.1.03 - Draggable Character Sheet Panels with mobile support
+document.querySelector("h1")?.insertAdjacentHTML("beforeend", " <span style='font-size:0.7em'>(v0.1.03)</span>");
 
 function log(msg) {
   const logArea = document.getElementById("debugLog") || (() => {
